ef0f2b9bd13027d79eeb239e86088454fdb0c4a1
Jeffrey <jeffrey.zhao@support.com>
2020-06-06 08:14:31 -0700

    'parameter'

--- process/clipextract.py
+++ process/clipextract.py
@@ -2,22 +2,26 @@ import pyperclip as clp
 import time
 import sys
 from importlib import import_module
-import f_extract
 
 # compare two and get the common in template, differ in parameter
 if __name__ == '__main__':
     clp.copy("")
     recent_value = ""
-    p = []
+    para = []
+    if len(sys.argv) == 2:
+        p, m = sys.argv[1].rsplit('.', 1)
+        mod = import_module(p)
+        transform = getattr(mod, m)
+
     for i in range(2):
         while True:
             tmp_value = clp.paste()
             if tmp_value != recent_value:
-                p.append(tmp_value)
+                para.append(tmp_value)
                 recent_value = tmp_value
                 break
             time.sleep(0.1)
-        print("Value '%s' has received."%(tmp_value[:20]+'......'))
-    output = f_extract.parameter(p[0],p[1])
+        print("Value '%s' has received."%(tmp_value[:10]+'......'))
+    output = transform(para[0],para[1])
     clp.copy(output)
     print(output)
