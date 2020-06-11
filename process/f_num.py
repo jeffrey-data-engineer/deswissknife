98cc5be62263aae4c7cafb400545799fc245726a
Jeffrey <jeffrey.zhao@support.com>
2020-06-10 21:48:59 -0700

    'update

--- process/f_num.py
+++ process/f_num.py
@@ -1,3 +1,4 @@
+import itertools
 def sublist(lststr, subtotal):
     if type(lststr)!=list:
         lststr = str(lststr).split(',')
@@ -6,6 +7,7 @@ def sublist(lststr, subtotal):
     else:
         lst = [i for i in lststr if i <=subtotal]
     return lst
+
 
 def bitwiseComplement(raw):
     """
@@ -37,4 +39,81 @@ def find_subtotal_composition(lststr, subtotal):
                     for i in r:
                         i.append(c)
                         w.append(i)
+    return w
+
+
+def dedup(w):
+    w.sort()
+    return list(w for w,_ in itertools.groupby(w))
+
+def ret_range(lst):
+    max = 0
+    min = 0
+    for i in lst:
+        if i>0:
+            max+=i
+        else:
+            min+=i
+    return max, min
+
+
+def find_subtotal_composition_float(lststr, subtotal):
+    if type(subtotal)!=float:
+        subtotal=float(subtotal)
+    if type(lststr)!=list:
+        lststr = str(lststr).split(',')
+        a = [float(i) for i in lststr]
+        a.sort()
+    else:
+        a = lststr
+    # print(a)
+    # print(subtotal)
+    w = []
+
+    sub = subtotal-a[0]
+
+    if abs(sub)<0.01:
+        w.append([a[0]])
+        return w
+    else:
+        if len(a)>1:
+            ma,mi = ret_range(a[1:])
+
+            #with this c included
+            if (sub<=ma and sub>=mi):
+                r = find_subtotal_composition_float(a[1:], sub)
+                if r and len(r)>0:
+                    #print("with:%s--%s--%s" %(a[1:],sub,r))
+                    for j in r:
+                        if len(j)>0:
+                            j.append(a[0])
+                            w.append(j)
+
+            #without this c included
+            if (subtotal<=ma and subtotal>=mi):
+                t = find_subtotal_composition_float(a[1:], subtotal)
+                if t and len(t)>0:
+                    #print("without:%s--%s---%s" %(a[1:],subtotal,t))
+                    for k in t:
+                        if len(k)>0:
+                            w.append(k)
+
+    return dedup(w)
+
+
+def find_subtotal(lststr, subtotal):
+    if type(subtotal)!=float:
+        subtotal=float(subtotal)
+    if type(lststr)!=list:
+        lststr = str(lststr).split(',')
+        a = [float(i) for i in lststr]
+    else:
+        a = lststr
+    w = []
+    for j in range(1,7):
+        t = dedup(list(itertools.combinations(a,j)))
+        #print(t)
+        for i in t:
+            if abs(sum(i)-subtotal)<0.01:
+                w.append(list(i))
     return w
--- test/test_f_num.py
+++ test/test_f_num.py
@@ -3,3 +3,7 @@ from deswissknife.process import f_num
 def test_find_subtotal_composition():
     actual = f_num.find_subtotal_composition('1,2,3,4',3)
     assert actual == [[3],[1,2]]
+
+def test_find_subtotal_composition_float():
+    actual = f_num.find_subtotal_composition_float('1.2,4,5,2.3,2.24',3.5)
+    assert actual == [[1.2,2.3]]
